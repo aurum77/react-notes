5dd1c9284147083721f1c4606dc4afe0c163812f
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-24 23:17:53 +0300

    Create prototype

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import { useState, useEffect } from "react";
 import notesService from "./services/notesService";
+import { Header, Footer, Note, Flex } from "./components";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    const notes = notesService.getAll().then((notes) => {
+    notesService.getAll().then((notes) => {
       setNotes(notes);
     });
   }, []);
 
   return (
     <div>
+      <Header />
       {notes.length === 0 ? (
         <div>Loading</div>
       ) : (
-        <div>
-          <ul>
-            {notes.map((note) => (
-              <li>{note.content}</li>
-            ))}
-          </ul>
-        </div>
+        <Flex>
+          {notes.map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
+        </Flex>
       )}
+      <Footer />
     </div>
   );
 };
