3257582fa8d619d71e788025148bb3c15047252f
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-24 20:01:22 +0300

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <footer className="footer__text">
+      ©️ 2022 Made with 💓 by aurum77
+    </footer>
+    </div>
+  );
+};
