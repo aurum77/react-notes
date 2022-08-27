60f8051330842ae56b3c018f2a2aad12b54ae7c2
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-27 09:41:51 +0300

    Add branding section to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,11 @@ import "./Header.css";
 export const Header = () => {
   return (
     <div className="header">
+      <img
+        className="header__branding"
+        src="https://icons.iconarchive.com/icons/mathijssen/tuxlets/256/Tux-icon.png"
+        alt="tux gang"
+      ></img>
       <div className="header__text">react-notes</div>
     </div>
   );
