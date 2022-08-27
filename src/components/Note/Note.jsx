f7c1772a046975c76ffa23b280ab357cf4207923
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-27 09:51:24 +0300

    Truncate longer Note content and title

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,10 +1,12 @@
 import "./Note.css";
 
 export const Note = ({ note }) => {
+  const content = note.content.length > 45 ? note.content.substr(0, 45) + "..." : note.content;
+  const title = note.content.length > 45 ? note.content.substr(0, 45) + "..." : note.content;
   return (
     <div className={`note ${note.color}`}>
-        <div className="note__title">{note.title}</div>
-        <div className={`note__content ${note.color}`}>{note.content}</div>
+        <div className="note__title">{title}</div>
+        <div className={`note__content ${note.color}`}>{content}</div>
       </div>
   );
 };
