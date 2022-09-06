8b62b012e32a3abda705fab630de557d2deb1a55
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-06 21:30:35 +0300

    Fix showing content instead of title

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,8 +1,10 @@
 import "./Note.css";
 
 export const Note = ({ note }) => {
+  // TODO: implement small text for note content if content is longer than 70 chars
+  // TODO: implement variable note size, 45 chars truncation is placeholder for now
   const content = note.content.length > 45 ? note.content.substr(0, 45) + "..." : note.content;
-  const title = note.content.length > 45 ? note.content.substr(0, 45) + "..." : note.content;
+  const title = note.title.length > 45 ? note.title.substr(0, 45) + "..." : note.title;
   return (
     <div className={`note ${note.color}`}>
         <div className="note__title">{title}</div>
