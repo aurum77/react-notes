35c6f2c7f51fbadcf227c50e1ad8048e33cb0cd8
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-06 12:15:55 +0300

    Add padding to header and align items center, change branding logo to default react logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <div className="header">
       <img
         className="header__branding"
-        src="https://icons.iconarchive.com/icons/mathijssen/tuxlets/256/Tux-icon.png"
+        src="./logo512.png"
         alt="tux gang"
       ></img>
       <div className="header__text">react-notes</div>
