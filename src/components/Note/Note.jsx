74ff597b6b8e216b1ca54aee42a87b147565c95b
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-08 00:11:14 +0300

    Increase truncation char length limit to 400 add small content size

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -3,12 +3,14 @@ import "./Note.css";
 export const Note = ({ note }) => {
   // TODO: implement small text for note content if content is longer than 70 chars
   // TODO: implement variable note size, 45 chars truncation is placeholder for now
-  const content = note.content.length > 45 ? note.content.substr(0, 45) + "..." : note.content;
-  const title = note.title.length > 45 ? note.title.substr(0, 45) + "..." : note.title;
+  const content = note.content.length > 400 ? note.content.substr(0, 400) + "..." : note.content;
+  const title = note.title.length > 400 ? note.title.substr(0, 400) + "..." : note.title;
+  const contentSize = note.content.length > 70 ? "note__content--small" : "";
+
   return (
     <div className={`note ${note.color}`}>
         <div className="note__title">{title}</div>
-        <div className={`note__content ${note.color}`}>{content}</div>
+        <div className={`note__content ${note.color} ${contentSize}`}>{content}</div>
       </div>
   );
 };
