fa4e3bb28ba9460fbd332728942cc38872f97173
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-10 00:36:35 +0300

    refactor: Overhaul NoteInput component

--- src/components/NoteInput/NoteInput.jsx
+++ src/components/NoteInput/NoteInput.jsx
@@ -1,17 +1,32 @@
 import "./NoteInput.css";
 
-export const NoteInput = ({ inputValue, onChange, onSubmit }) => {
+export const NoteInput = ({
+  inputTitle,
+  inputContent,
+  handleInputTitleChange,
+  handleInputContentChange,
+  onSubmit,
+}) => {
   return (
     <form onSubmit={onSubmit}>
       <div className="noteInput">
-        <input
-          className="noteInput__input"
-          type="text"
-          placeholder="Take a note..."
-          value={inputValue}
-          onChange={onChange}
-        />
-        <button>submit</button>
+        <div className="noteInput__box">
+          <input
+            className="noteInput__title"
+            type="text"
+            placeholder="Title"
+            value={inputTitle}
+            onChange={handleInputTitleChange}
+          />
+          <input
+            className="noteInput__content"
+            type="text"
+            placeholder="Take a note..."
+            value={inputContent}
+            onChange={handleInputContentChange}
+          />
+          <button className="noteInput__button">Submit</button>
+        </div>
       </div>
     </form>
   );
