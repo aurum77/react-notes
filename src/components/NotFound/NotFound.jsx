979fd1e11698317d0c10b559cec3c1d29584b4ce
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-13 23:59:46 +0300

    feat: add not found page

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import "./NotFound.css";
+
+export const NotFound = () => {
+  return (
+    <div className="notFound">
+      <img
+        className="notFound__img"
+        src="https://http.cat/404"
+        alt="a cat hiding under some paper"
+      />
+    </div>
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { Header } from "./Header";
 export { Note } from "./Note";
 export { Notes } from "./Notes";
 export { NoteInput } from "./NoteInput";
+export { NoteInputToolbar } from "./NoteInputToolbar";
+export { NotFound } from "./NotFound";
