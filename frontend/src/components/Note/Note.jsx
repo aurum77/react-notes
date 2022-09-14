40997207aa9e3a07beccabee06b8cd71a6b1f945
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-14 21:38:47 +0300

    style: formatting

--- frontend/src/components/Note/Note.jsx
+++ frontend/src/components/Note/Note.jsx
@@ -1,14 +1,18 @@
 import "./Note.css";
 
 export const Note = ({ note }) => {
-  const content = note.content.length > 400 ? note.content.substr(0, 400) + "..." : note.content;
-  const title = note.title.length > 400 ? note.title.substr(0, 400) + "..." : note.title;
+  const content =
+    note.content.length > 400
+      ? note.content.substr(0, 400) + "..."
+      : note.content;
+  const title =
+    note.title.length > 400 ? note.title.substr(0, 400) + "..." : note.title;
   const contentSize = note.content.length > 70 ? "note__content--small" : "";
 
   return (
     <div className={`note ${note.color}`}>
-        <div className="note__title">{title}</div>
-        <div className={`note__content ${contentSize}`}>{content}</div>
+      <div className="note__title">{title}</div>
+      <div className={`note__content ${contentSize}`}>{content}</div>
       </div>
   );
 };
