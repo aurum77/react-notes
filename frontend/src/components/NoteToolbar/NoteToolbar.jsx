ca74743d12321874db5dbad4d44745b04bc6be5b
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-15 11:48:01 +0300

    frontend:feat: implement note deletion

--- frontend/src/components/NoteToolbar/NoteToolbar.jsx
+++ frontend/src/components/NoteToolbar/NoteToolbar.jsx
@@ -1,15 +1,25 @@
 import "./NoteToolbar.css";
+import { useContext } from "react";
+import notesService from "../../services/notesService";
+import NotesContext from "../../contexts/NotesContext";
 
-export const NoteToolbar = () => {
+export const NoteToolbar = ({ id }) => {
+  const { setNotes } = useContext(NotesContext);
+
+  const handleNoteDeleteAction = () => {
+    notesService.deleteNote(id).then((notes) => {
+      setNotes(notes);
+    });
+  };
+
   return (
     <div className="noteToolbar">
-      <div className="noteToolbar__element">
+      <div className="noteToolbar__element" onClick={handleNoteDeleteAction}>
         <img
           src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
           alt="delete a note"
         />
       </div>
-
     </div>
   );
 };
