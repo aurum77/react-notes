0c87d85f8704421a3cb6bdb23a077c6768b1c730
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-15 11:40:57 +0300

    frontend:feat: show a message if there aren't any notes

--- frontend/src/components/Notes/Notes.jsx
+++ frontend/src/components/Notes/Notes.jsx
@@ -1,5 +1,10 @@
 import "./Notes.css";
 
-export const Notes = (props) => {
-  return <div className="notes">{props.children}</div>;
+export const Notes = ({children}) => {
+  return(
+    <div className="notes">
+      {(children.length !== 0 ? children : "You don't have any notes")}
+    {console.log(children)}
+    </div>
+  )
 };
