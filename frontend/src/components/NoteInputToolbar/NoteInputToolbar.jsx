08d6aa9aa263be270edffb4bc3a162dd8bc88c02
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-15 11:26:02 +0300

    frontend:refactor: use color prop instead of abusing id

--- frontend/src/components/NoteInputToolbar/NoteInputToolbar.jsx
+++ frontend/src/components/NoteInputToolbar/NoteInputToolbar.jsx
@@ -6,7 +6,7 @@ export const NoteInputToolbar = ({ children }) => {
   const { noteColor, setNoteColor } = useContext(NoteContext);
 
   const handleColorPick = (event) => {
-    setNoteColor(event.target.id);
+    setNoteColor(event.target.getAttribute("color"));
   };
 
   return (
@@ -15,28 +15,28 @@ export const NoteInputToolbar = ({ children }) => {
         className={`noteInputToolbar__dot ${
           noteColor === "" ? "noteInputToolbar__dot--selected" : ""
         }`}
-        id=""
+        color=""
         onClick={handleColorPick}
       ></span>
       <span
         className={`noteInputToolbar__dot noteInputToolbar__dot--blue ${
           noteColor === "note--blue" ? "noteInputToolbar__dot--selected" : ""
         }`}
-        id="note--blue"
+        color="note--blue"
         onClick={handleColorPick}
       />
       <span
         className={`noteInputToolbar__dot noteInputToolbar__dot--red ${
           noteColor === "note--red" ? "noteInputToolbar__dot--selected" : ""
         }`}
-        id="note--red"
+        color="note--red"
         onClick={handleColorPick}
       />
       <span
         className={`noteInputToolbar__dot noteInputToolbar__dot--purple ${
           noteColor === "note--purple" ? "noteInputToolbar__dot--selected" : ""
         }`}
-        id="note--purple"
+        color="note--purple"
         onClick={handleColorPick}
       />
       <span
@@ -45,28 +45,28 @@ export const NoteInputToolbar = ({ children }) => {
             ? "noteInputToolbar__dot--selected"
             : ""
         }`}
-        id="note--turquoise"
+        color="note--turquoise"
         onClick={handleColorPick}
       />
       <span
         className={`noteInputToolbar__dot noteInputToolbar__dot--green ${
           noteColor === "note--green" ? "noteInputToolbar__dot--selected" : ""
         }`}
-        id="note--green"
+        color="note--green"
         onClick={handleColorPick}
       />
       <span
         className={`noteInputToolbar__dot noteInputToolbar__dot--yellow ${
           noteColor === "note--yellow" ? "noteInputToolbar__dot--selected" : ""
         }`}
-        id="note--yellow"
+        color="note--yellow"
         onClick={handleColorPick}
       />
       <span
         className={`noteInputToolbar__dot noteInputToolbar__dot--orange ${
           noteColor === "note--orange" ? "noteInputToolbar__dot--selected" : ""
         }`}
-        id="note--orange"
+        color="note--orange"
         onClick={handleColorPick}
       />
       <div className="noteInputToolbar__filler"></div>
