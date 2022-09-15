db24d1e1f6c3fc3c31039031e4e0409e67a05f0a
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-15 11:44:39 +0300

    frontend:feat: add loading state

--- frontend/src/contexts/NotesContext.jsx
+++ frontend/src/contexts/NotesContext.jsx
@@ -5,16 +5,20 @@ const NotesContext = createContext();
 
 export const NotesProvider = ({ children }) => {
   const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     notesService.getAll().then((notes) => {
       setNotes(notes);
+      setLoading(false);
     });
   }, []);
 
   const state = {
     notes,
     setNotes,
+    loading,
+    setLoading,
   };
 
   return (
