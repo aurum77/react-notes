82c196de37384d6d2cc07b9a965db0b34bde35c9
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-16 14:19:35 +0300

    frontend: remove classname from footer text

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <div className="footer">
-      <footer className="footer__text">
+      <footer>
       ©️ 2022 Made with 💓 by aurum77
     </footer>
     </div>
