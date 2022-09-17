7ba79abf41b0dfa6fd0a591cdaa6a7ea63cac6f0
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-17 19:33:52 +0300

    frontend:refactor: move note section headers to indexpage

--- frontend/src/components/Notes/Notes.jsx
+++ frontend/src/components/Notes/Notes.jsx
@@ -1,17 +1,9 @@
 import "./Notes.css";
 
 export const Notes = ({ children }) => {
-  const notes = children.filter((child) => child.props.note.pinned !== true);
-  const pinned = children.filter((child) => child.props.note.pinned === true);
-
   return (
     <div>
-      <div className="notes__header">PINNED</div>
-      <div className="notes__container">
-        {pinned.length !== 0 ? pinned : ""}
-      </div>
-      <div className="notes__header">NOTES</div>
-      <div className="notes__container">{notes.length !== 0 ? notes : ""}</div>
+      <div className="notes__container">{children.length !== 0 ? children : ""}</div>
     </div>
   );
 };
--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -4,18 +4,31 @@ import NotesContext from "../contexts/NotesContext";
 
 export const IndexPage = () => {
   const { notes, loading } = useContext(NotesContext);
+  const notPinnedNotes = notes.filter(
+    (note) => note.pinned !== true
+  );
+  const pinnedNotes = notes.filter((note) => note.pinned === true);
 
   return (
     <div>
       <NoteInput />
-      {!loading ? (
-        <Notes>
-          {notes.map((note) => (
-            <Note key={note.id} note={note} />
-          ))}
-        </Notes>
-      ) : (
+      {loading ? (
         <Loading />
+      ) : (
+        <div>
+          <div className="notes__header">PINNED</div>
+          <Notes>
+            {pinnedNotes.map((note) => (
+              <Note key={note.id} note={note} />
+            ))}
+          </Notes>
+          <div className="notes__header">NOTES</div>
+          <Notes>
+            {notPinnedNotes.map((note) => (
+              <Note key={note.id} note={note} />
+            ))}
+          </Notes>
+        </div>
       )}
     </div>
   );
