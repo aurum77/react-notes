47c02868ad255460b72979ad91550fd96a76dfd5
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-17 11:00:30 +0300

    frontend:fix: implement focused style for headersearchinput

--- frontend/src/components/HeaderSearchInput/HeaderSearchInput.jsx
+++ frontend/src/components/HeaderSearchInput/HeaderSearchInput.jsx
@@ -1,13 +1,26 @@
 import "./HeaderSearchInput.css";
+import { useState } from "react";
 
 export const HeaderSearchInput = () => {
+  const [focused, setFocused] = useState(false);
+
+  const handleSearchBoxFocus = () => {
+    setFocused(!focused);
+  };
+
   return (
     <div className="noteSearchInput">
-    <div className="noteSearchInput__box">
-      <div className="noteSearchInput__icon material-symbols-outlined">search</div>
-      <input className="noteSearchInput__search" placeholder="Search for notes" />
-    </div>
-
+      <div className={`noteSearchInput__box ${focused ? "noteSearchInput__box--focused" : ""}`}>
+        <div className="noteSearchInput__icon material-symbols-outlined">
+          search
+        </div>
+        <input
+          className="noteSearchInput__search"
+          placeholder="Search for notes"
+          onFocus={handleSearchBoxFocus}
+          onBlur={handleSearchBoxFocus}
+        />
+      </div>
     </div>
   );
 };
