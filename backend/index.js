c138ecd1639b90864b4a1be3baa8c98871832e0a
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-19 22:36:48 +0300

    backend:feat: mongodb integration

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 const json = express.json();
 const port = 3001;
+
+const Note = require("./models/note");
 
 app.use(json);
 app.use(cors());
@@ -11,144 +14,25 @@ app.use(cors());
 morgan.token("json", (request, response) => {
   return JSON.stringify(request.body);
 });
+
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :json")
 );
 
-let notes = [
-  {
-    id: 1,
-    title: "this is a title",
-    content: "this is a note",
-    color: "note--turquoise",
-    pinned: true,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 20 2022 15:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 2,
-    title: "",
-    content: "im a cat",
-    color: "note--green",
-    pinned: false,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 12 2022 18:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 3,
-    title: "",
-    content: "meow",
-    color: "note--turquoise",
-    pinned: false,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 13 2022 15:45:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 4,
-    title: "",
-    content: "yellow cat",
-    color: "note--yellow",
-    pinned: false,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 14 2022 11:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 5,
-    title: "",
-    content: "hello from express",
-    color: "note--orange",
-    pinned: true,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 12 2022 11:48:06",
-    edited: "Sat Aug 12 2022 11:48:06",
-  },
-  {
-    id: 6,
-    title: "",
-    content: "hello from express",
-    color: "note--darkblue",
-    pinned: true,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 12 2022 11:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 7,
-    title: "",
-    content:
-      "wow continuous text AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-    color: "note--turquoise",
-    pinned: true,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 12 2022 11:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 8,
-    title: "",
-    content: "some text shorter than 70 chars expect normal font",
-    color: "",
-    pinned: true,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 12 2022 11:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 9,
-    title: "unnecessarily long text for checking overflows",
-    content:
-      "Do not use the same id when lazily copying and pasting this json object CLARKSOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON",
-    color: "note--red",
-    pinned: true,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 12 2022 11:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-  {
-    id: 10,
-    title: "",
-    content:
-      "some text longer than 70 chars lalalalalalalalalalalalalalalalalalalala expect small font",
-    color: "note--turquoise",
-    pinned: true,
-    trashed: false,
-    archived: false,
-    created: "Sat Aug 12 2022 11:48:06",
-    edited: "Sat Aug 20 2022 15:48:06",
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
-
 app.get("/api/notes", (req, res) => {
-  return res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.post("/api/notes", (req, res) => {
-  const props = req.body;
-
-  let noteId = Math.floor(Math.random() * 100000);
-  while (notes.find((note) => note.id === noteId)) {
-    noteId = Math.floor(Math.random() * 100000);
+  if(req.body.content === undefined) {
+    return res.status(400).json({error: "content missing"});
   }
 
-  const newNote = {
-    id: noteId,
+  const props = req.body;
+
+  const newNote = new Note({
     title: props.title,
     color: props.color,
     content: props.content,
@@ -157,26 +41,24 @@ app.post("/api/notes", (req, res) => {
     archived: props.archived,
     created: new Date(),
     edited: new Date(),
-  };
+    tags: props.tags,
+  });
 
-  notes.push(newNote);
+  newNote.save();
   return res.status(201).json(newNote);
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-  const noteId = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== noteId);
-  return res.status(200).json(notes);
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id).then(() => {
+    res.status(204).end();
+  })
+  .catch(error => next(error));
 });
 
 app.patch("/api/notes/:id", (req, res) => {
-  const noteId = Number(req.params.id);
-  const newNote = 
-  notes = [
-    ...notes,
-
-  ]
-})
+  Note.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => res.status(200).end())
+});
 
 app.listen(port, () => {
   console.log(`Express app running on port ${port}`);
---
+++ backend/models/note.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(`error connecting to MongoDB: ${err.message}`);
+  });
+
+const noteSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  content: String,
+  color: String,
+  pinned: Boolean,
+  trashed: Boolean,
+  archived: Boolean,
+  created: Date,
+  edited: Date,
+  tags: [String],
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
