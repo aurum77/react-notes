6392b5f6514867af040b9aa853dc9bfee4afcedd
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-19 15:31:17 +0300

    frontend:refactor: move colorpicker to its own component

---
+++ frontend/src/components/Common/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,74 @@
+import "./ColorPicker.css";
+import { useContext } from "react";
+import NoteInputContext from "../../../contexts/NoteInputContext";
+
+export const ColorPicker = () => {
+  const { noteColor, setNoteColor } = useContext(NoteInputContext);
+
+  const handleColorPick = (event) => {
+    setNoteColor(event.target.getAttribute("color"));
+  };
+
+  return (
+    <>
+      <span
+        className={`colorPicker__dot material-symbols-outlined ${
+          noteColor === "" ? "colorPicker__dot--selected" : ""
+        }`}
+        color=""
+        onClick={handleColorPick}
+      ></span>
+      <span
+        className={`colorPicker__dot material-symbols-outlined colorPicker__dot--blue ${
+          noteColor === "note--blue" ? "colorPicker__dot--selected" : ""
+        }`}
+        color="note--blue"
+        onClick={handleColorPick}
+      />
+      <span
+        className={`colorPicker__dot material-symbols-outlined colorPicker__dot--red ${
+          noteColor === "note--red" ? "colorPicker__dot--selected" : ""
+        }`}
+        color="note--red"
+        onClick={handleColorPick}
+      />
+      <span
+        className={`colorPicker__dot material-symbols-outlined colorPicker__dot--purple ${
+          noteColor === "note--purple" ? "colorPicker__dot--selected" : ""
+        }`}
+        color="note--purple"
+        onClick={handleColorPick}
+      />
+      <span
+        className={`colorPicker__dot material-symbols-outlined colorPicker__dot--turquoise ${
+          noteColor === "note--turquoise"
+            ? "colorPicker__dot--selected"
+            : ""
+        }`}
+        color="note--turquoise"
+        onClick={handleColorPick}
+      />
+      <span
+        className={`colorPicker__dot material-symbols-outlined colorPicker__dot--green ${
+          noteColor === "note--green" ? "colorPicker__dot--selected" : ""
+        }`}
+        color="note--green"
+        onClick={handleColorPick}
+      />
+      <span
+        className={`colorPicker__dot material-symbols-outlined colorPicker__dot--yellow ${
+          noteColor === "note--yellow" ? "colorPicker__dot--selected" : ""
+        }`}
+        color="note--yellow"
+        onClick={handleColorPick}
+      />
+      <span
+        className={`colorPicker__dot material-symbols-outlined colorPicker__dot--orange ${
+          noteColor === "note--orange" ? "colorPicker__dot--selected" : ""
+        }`}
+        color="note--orange"
+        onClick={handleColorPick}
+      />
+    </>
+  );
+};
--- frontend/src/components/NoteInputToolbar/NoteInputToolbar.jsx
+++ frontend/src/components/NoteInputToolbar/NoteInputToolbar.jsx
@@ -1,76 +1,13 @@
 import "./NoteInputToolbar.css";
-import { useContext } from "react";
-import NoteInputContext from "../../contexts/NoteInputContext";
+import { ColorPicker } from "../Common/ColorPicker";
 
 export const NoteInputToolbar = ({ children }) => {
-  const { noteColor, setNoteColor } = useContext(NoteInputContext);
-
-  const handleColorPick = (event) => {
-    setNoteColor(event.target.getAttribute("color"));
-  };
-
   return (
     <div className="noteInputToolbar">
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined ${
-          noteColor === "" ? "noteInputToolbar__dot--selected" : ""
-        }`}
-        color=""
-        onClick={handleColorPick}
-      ></span>
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined noteInputToolbar__dot--blue ${
-          noteColor === "note--blue" ? "noteInputToolbar__dot--selected" : ""
-        }`}
-        color="note--blue"
-        onClick={handleColorPick}
-      />
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined noteInputToolbar__dot--red ${
-          noteColor === "note--red" ? "noteInputToolbar__dot--selected" : ""
-        }`}
-        color="note--red"
-        onClick={handleColorPick}
-      />
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined noteInputToolbar__dot--purple ${
-          noteColor === "note--purple" ? "noteInputToolbar__dot--selected" : ""
-        }`}
-        color="note--purple"
-        onClick={handleColorPick}
-      />
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined noteInputToolbar__dot--turquoise ${
-          noteColor === "note--turquoise"
-            ? "noteInputToolbar__dot--selected"
-            : ""
-        }`}
-        color="note--turquoise"
-        onClick={handleColorPick}
-      />
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined noteInputToolbar__dot--green ${
-          noteColor === "note--green" ? "noteInputToolbar__dot--selected" : ""
-        }`}
-        color="note--green"
-        onClick={handleColorPick}
-      />
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined noteInputToolbar__dot--yellow ${
-          noteColor === "note--yellow" ? "noteInputToolbar__dot--selected" : ""
-        }`}
-        color="note--yellow"
-        onClick={handleColorPick}
-      />
-      <span
-        className={`noteInputToolbar__dot material-symbols-outlined noteInputToolbar__dot--orange ${
-          noteColor === "note--orange" ? "noteInputToolbar__dot--selected" : ""
-        }`}
-        color="note--orange"
-        onClick={handleColorPick}
-      />
+      <ColorPicker />
       <div className="noteInputToolbar__filler"></div>
       {children}
+      <button className={`noteInput__button`}>Close</button>
     </div>
   );
 };
