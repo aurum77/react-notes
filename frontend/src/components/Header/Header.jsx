a80d6f104ebfeb9a352c65b8b112dd25deeddbb6
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-22 22:33:54 +0300

    feat:frontend: add menu button to header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,10 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <div className="header">
-      <img className="header__branding" src="./logo192.png" alt="branding" />
+    <div className="header__hamburger material-symbols-outlined">
+      menu
+    </div>
+      <img className="header__branding" src="./logo192.png" alt="Header branding image" />
       <Link to="/" style={{ textDecoration: "none" }}>
         <div className="header__text">react-notes</div>
       </Link>
