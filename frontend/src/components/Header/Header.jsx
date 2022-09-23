32be348287869dcd0d474938fbbe98edc7722b19
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-23 21:23:38 +0300

    frontend:refactor: refactor header styles

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,25 +5,15 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <div className="header">
-      <div className="header__leftmost material-symbols-outlined">menu</div>
-      <img
-        className="header__branding"
-        src="./logo192.png"
-        alt="Header branding image"
-      />
+    <div className="header__hamburger material-symbols-outlined">
+      menu
+    </div>
+      <img className="header__branding" src="./logo192.png" alt="Header branding image" />
       <Link to="/" style={{ textDecoration: "none" }}>
         <div className="header__text">react-notes</div>
       </Link>
       <div className="header__item--fill">
-        <HeaderSearch />
-      </div>
-      <div className="header__item material-symbols-outlined">archive</div>
-      <div className="header__item material-symbols-outlined">delete</div>
-      <div className="header__rightmost">
-        <img
-          className="header__usericon"
-          src="https://external-preview.redd.it/liW4TFEad0ZONXF01u6gX8PLr9lvJT-qTK6OaW25cG4.jpg?auto=webp&s=461f514f48cd84227a7cb287a51a2648469dc9a1"
-        />
+          <HeaderSearchInput />
       </div>
     </div>
   );
