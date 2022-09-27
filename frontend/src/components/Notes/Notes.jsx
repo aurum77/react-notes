c34da1d23e404fe37d2f390208bd6eb33af2c938
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-27 21:07:38 +0300

    frontend:refactor: remove check for children

--- frontend/src/components/Notes/Notes.jsx
+++ frontend/src/components/Notes/Notes.jsx
@@ -3,7 +3,9 @@ import "./Notes.css";
 export const Notes = ({ children }) => {
   return (
     <div>
-      <div className="notes__container">{children.length !== 0 ? children : ""}</div>
+      <div className="notes__container">
+        {children}
+      </div>
     </div>
   );
 };
