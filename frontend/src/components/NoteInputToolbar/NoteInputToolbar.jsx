c5972347bcfbebb6f0df9d13924a6d3ac82792e7
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-27 22:07:07 +0300

    frontend:feat: add styles for buttons in note input form

--- frontend/src/components/NoteInputToolbar/NoteInputToolbar.jsx
+++ frontend/src/components/NoteInputToolbar/NoteInputToolbar.jsx
@@ -4,7 +4,19 @@ import { ColorPicker } from "../Common/ColorPicker";
 export const NoteInputToolbar = () => {
   return (
     <div className="noteInputToolbar">
-      <ColorPicker />
+      <div className="noteInputToolbar__element material-symbols-outlined">
+        push_pin
+      </div>
+      <div className="noteInputToolbar__element material-symbols-outlined">
+        archive
+      </div>
+      <div className="noteInputToolbar__element material-symbols-outlined">
+        palette
+      </div>
+      <div className="noteInputToolbar__element material-symbols-outlined">
+        add_photo_alternate
+      </div>
+      {/* <ColorPicker /> */}
       <div className="noteInputToolbar__filler" />
       <button className={`noteInput__button`}>Close</button>
     </div>
