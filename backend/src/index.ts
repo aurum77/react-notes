e7538bc554546b287002a1d1aa21cc80dcebaaed
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-10-07 15:53:11 +0300

    backend: migrate index.js to typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+require("dotenv").config()
+import express, { Request, Application } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import noteRoutes from "./routes/note";
+
+const app: Application = express();
+const json = express.json();
+
+const port = 3001;
+
+app.use(json);
+app.use(cors());
+
+morgan.token("json", (request: Request) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :json")
+);
+
+// Put middleware before this line
+app.use("/api", noteRoutes);
+
+app.listen(port, () => {
+  console.log(`Express app running on port ${port}`);
+});
