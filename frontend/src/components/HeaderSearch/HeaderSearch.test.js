742e3c039bd10dc77aadbadcdfc59c0a68b700ac
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-10-10 23:32:17 +0300

    backend: add test for notesearch component

---
+++ frontend/src/components/HeaderSearch/HeaderSearch.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import SearchContext from "../../contexts/SearchContext";
+import { HeaderSearch } from "./HeaderSearch";
+
+const searchState = {
+  searchValue: "foo",
+};
+
+const renderHeaderSearch = () => {
+  return render(
+    <SearchContext.Provider value={searchState}>
+      <HeaderSearch />
+    </SearchContext.Provider>
+  );
+};
+
+describe("HeaderSearch", () => {
+  it("should have a search icon", () => {
+    renderHeaderSearch();
+    const searchIcon = screen.getByText("search");
+    expect(searchIcon).toBeInTheDocument;
+  });
+
+  it("should have a search input", () => {
+    renderHeaderSearch();
+    const searchInput = screen.getByPlaceholderText(/search for notes/i);
+    expect(searchInput).toBeInTheDocument;
+  });
+});
