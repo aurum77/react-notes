3443b0a658e9ab4e0f2e1550ab3924cb2eb6c9b9
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-10-11 20:20:52 +0300

    frontend: migrate footer component to typescript

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
+import { FunctionComponent, ReactElement } from "react";
 import "./Footer.css";
 
-export const Footer = () => {
+export const Footer: FunctionComponent = (): ReactElement  => {
   return (
     <div className="footer">
       <footer>&#169; 2022 Made with 💓 by aurum77</footer>
