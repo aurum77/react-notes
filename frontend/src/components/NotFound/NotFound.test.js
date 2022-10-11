6812847eb53b1c0cb11f87737fd0ea4b1125a48a
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-10-11 16:58:38 +0300

    frontend:test: add test for text in notfound component

--- frontend/src/components/NotFound/NotFound.test.js
+++ frontend/src/components/NotFound/NotFound.test.js
@@ -7,4 +7,10 @@ describe("NotFound", () => {
     const notfoundImg = screen.getAllByAltText("a cat hiding under some paper");
     expect(notfoundImg).toBeInTheDocument;
   });
+
+  it("should show the not found text", () => {
+    render(<NotFound />);
+    const notFoundText = screen.getByText(/not found/i);
+    expect(notFoundText).toBeInTheDocument;
+  })
 });
