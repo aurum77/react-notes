e90a3f062dfa79aa0dbac9b90d6bbd5ce8f2c69b
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-10-11 17:36:19 +0300

    frontend:feat: implement route based notetoolbar items

--- frontend/src/components/NoteToolbar/NoteToolbar.jsx
+++ frontend/src/components/NoteToolbar/NoteToolbar.jsx
@@ -1,10 +1,12 @@
 import "./NoteToolbar.css";
 import { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import notesService from "../../services/notesService";
 import NotesContext from "../../contexts/NotesContext";
 
 export const NoteToolbar = ({ id, visibility }) => {
   const { notes, setNotes } = useContext(NotesContext);
+  const location = useLocation();
 
   const isPinned = (id) => {
     const filteredNote = notes.filter((note) => note.id === id)[0];
@@ -15,8 +17,8 @@ export const NoteToolbar = ({ id, visibility }) => {
     const filteredNote = notes.filter((note) => note.id === id)[0];
     filteredNote.trashed = !filteredNote.trashed;
 
-    filteredNote.archived = false
-    filteredNote.pinned = false
+    filteredNote.archived = false;
+    filteredNote.pinned = false;
 
     const newNotes = notes.filter((note) => note.id !== id);
 
@@ -53,40 +55,69 @@ export const NoteToolbar = ({ id, visibility }) => {
     setNotes([...newNotes, filteredNote]);
   };
 
-  return (
-    <div className={`noteToolbar ${visibility ? "noteToolbar--visible" : "noteToolbar--hidden"}`}>
-      <div
-        className={`noteToolbar__element material-symbols-outlined ${
-          isPinned(id) ? "noteToolbar__element--filled" : ""
-        }`}
-        onClick={handleNoteTogglePinned}
-      >
-        push_pin
-      </div>
-      <div
-        className="noteToolbar__element material-symbols-outlined"
-        onClick={handleNoteToggleArchived}
-      >
-        archive
-      </div>
-      <div
-        className="noteToolbar__element material-symbols-outlined"
-        onClick={handleNoteToggleTrashed}
-      >
-        delete
-      </div>
-      <div
-        className="noteToolbar__element material-symbols-outlined"
-        onClick={handleNoteToggleTrashed}
-      >
-        palette
-      </div>
-      <div
-        className="noteToolbar__element material-symbols-outlined"
-        onClick={handleNoteToggleTrashed}
-      >
-        add_photo_alternate
-      </div>
-    </div>
-  );
+  switch (location.pathname) {
+    default:
+      return (
+        <div
+          className={`noteToolbar ${
+            visibility ? "noteToolbar--visible" : "noteToolbar--hidden"
+          }`}
+        >
+          <div
+            className={`noteToolbar__element material-symbols-outlined ${
+              isPinned(id) ? "noteToolbar__element--filled" : ""
+            }`}
+            onClick={handleNoteTogglePinned}
+          >
+            push_pin
+          </div>
+          <div
+            className="noteToolbar__element material-symbols-outlined"
+            onClick={handleNoteToggleArchived}
+          >
+            archive
+          </div>
+          <div
+            className="noteToolbar__element material-symbols-outlined"
+            onClick={handleNoteToggleTrashed}
+          >
+            delete
+          </div>
+          <div
+            className="noteToolbar__element material-symbols-outlined"
+            onClick={handleNoteToggleTrashed}
+          >
+            palette
+          </div>
+          <div
+            className="noteToolbar__element material-symbols-outlined"
+            onClick={handleNoteToggleTrashed}
+          >
+            add_photo_alternate
+          </div>
+        </div>
+      );
+
+    case "/trashed":
+      return (
+        <div
+          className={`noteToolbar ${
+            visibility ? "noteToolbar--visible" : "noteToolbar--hidden"
+          }`}
+        >
+          <div
+            className="noteToolbar__element material-symbols-outlined"
+            onClick={handleNoteToggleTrashed}
+          >
+            restore_from_trash
+          </div>
+          <div
+            className="noteToolbar__element material-symbols-outlined"
+            onClick={handleNoteToggleTrashed}
+          >
+            delete_forever
+          </div>
+        </div>
+      );
+  }
 };
