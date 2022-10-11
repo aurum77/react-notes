7f12116d7fa224476d3088b2aacff9bcfe370260
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-10-11 16:58:19 +0300

    frontend: add text to notfound component

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -2,12 +2,15 @@ import "./NotFound.css";
 
 export const NotFound = () => {
   return (
-    <div className="notFound">
-      <img
-        className="notFound__img"
-        src="https://http.cat/404"
-        alt="a cat hiding under some paper"
-      />
-    </div>
+    <>
+      <div className="notFound">
+        <img
+          className="notFound__img"
+          src="https://http.cat/404"
+          alt="a cat hiding under some paper"
+        />
+      </div>
+      <div className="notFound__text">Not found</div>
+    </>
   );
 };
