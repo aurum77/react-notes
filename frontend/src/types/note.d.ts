d138494f4860a8b26b931e2eba2db9bde2e01910
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-10-16 17:22:31 +0300

    frontend: add Note type

---
+++ frontend/src/types/note.d.ts
@@ -0,0 +1,12 @@
+type Note = {
+  title: string;
+  content: string;
+  color: string;
+  pinned: boolean;
+  trashed: boolean;
+  archived: boolean;
+  created: Date;
+  edited: Date;
+  tags: string[];
+  id: string;
+}
